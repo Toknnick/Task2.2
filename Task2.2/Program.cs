23be56e4237830da4c81579a1b96f131872e2eb6
Maksim <maksimus.tumannick@yandex.ru>
2022-06-24 15:22:43 +0300

    Task2.2

---
+++ Task2.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputWord = "";
+            string stopWord = "exit";
+
+            while (inputWord != stopWord)
+            {
+                Console.WriteLine("Введите стоп слово");
+                inputWord = Console.ReadLine();
+            }
+        }
+    }
+}
